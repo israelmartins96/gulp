82671a3c0c6259d8f32681278a40c87594241a8f
Israel Martins <m.oisrael96@gmail.com>
2024-08-20 22:56:35 +0100

    Set Up BrowserSync File Watch and Sync

--- gulpfile.js
+++ gulpfile.js
@@ -38,11 +38,21 @@ import source from 'vinyl-source-stream';
 // Vinyl Buffer.
 import buffer from 'vinyl-buffer';
 
+// Browser Sync.
+import theBrowserSync, { watch } from 'browser-sync';
+
+const browserSync = theBrowserSync.create();
+// BrowserSync Reload.
+const reload = browserSync.reload;
+
 /**
  * Stylesheets and scripts sources and destinations.
  */
 const paths = {
     current: '.',
+    html: {
+        watch: './**/*.html'
+    },
     styles: {
         src: {
             main: 'src/scss/style.scss',
@@ -64,26 +74,30 @@ const paths = {
         },
         dest: './dist/js/',
         watch: 'src/js/**/*.js'
+    },
+    php: {
+        watch: './**/*.php'
     }
 };
 
 /**
- * Tasks list.
+ * Tasks data.
  */
+// Tasks list.
 const tasksList = {
     default: 'default',
     style: 'style',
     js: 'js',
-    watch: 'watch'
+    watch: 'watch',
+    browserSync: 'browser-sync'
 };
 
-/**
- * Default tasks.
- */
+// Default tasks.
 const tasksDefault = [
     tasksList.style,
     tasksList.js,
-    tasksList.watch
+    tasksList.watch,
+    tasksList.browserSync
 ];
 
 /**
@@ -105,7 +119,9 @@ const loadStyle = async () => {
     // Add the ".min" suffix to the file name.
     .pipe( rename( { suffix: '.min' } ) )
     // Render the CSS file in the CSS directory with external sourcemaps.
-    .pipe( gulp.dest( paths.styles.dest, { sourcemaps: '.' } ) );
+    .pipe( gulp.dest( paths.styles.dest, { sourcemaps: '.' } ) )
+    // Add BrowserSync stream.
+    .pipe( browserSync.stream() );
 };
 
 /**
@@ -131,7 +147,9 @@ const mapScripts = entryScript => {
         // Uglify.
         .pipe( uglify() )
         // Render the JavaScript file in the directory with external sourcemaps.
-        .pipe( gulp.dest( paths.scripts.dest, { sourcemaps: '.' } ) );
+        .pipe( gulp.dest( paths.scripts.dest, { sourcemaps: '.' } ) )
+        // Add BrowserSync stream.
+        .pipe( browserSync.stream() );
 };
 
 /**
@@ -145,28 +163,52 @@ const loadJavaScript = async () => {
  * Watches for specified tasks.
  */
 const doWatch = () => {
-    // Watch for style update.
+    // Watch for style updates.
     gulp.watch( paths.styles.watch, loadStyle );
-    // Watch for script update.
+    // Watch for script updates.
     gulp.watch( paths.scripts.watch, loadJavaScript );
+    // Watch for HTML updates.
+    gulp.watch( paths.html.watch, reload );
+    // Watch for PHP updates.
+    gulp.watch( paths.php.watch, reload );
 };
 
 /**
- * Task to compile stylesheet.
+ * Browser Sync.
  */
+const doBrowserSync = () => {
+    browserSync.init( {
+        open: false,
+        injectChanges: true,
+        proxy: 'http://127.0.0.1/mysandbox/javascript/gulp/',
+        watch: true
+    } );
+
+    /**
+     * Only use when not using the Gulp 'watch' task (i.e., when using 'browser-sync' task):
+     */
+    // // Watch for style updates.
+    // gulp.watch( paths.styles.watch, loadStyle );
+    // // Watch for script updates.
+    // gulp.watch( paths.scripts.watch, loadJavaScript );
+    // // Watch for updates in the HTML files.
+    // gulp.watch( paths.html.watch ).on( 'change', reload );
+};
+
+/**
+ * Tasks.
+ */
+// For Browser Sync. "gulp browser-sync" in CLI.
+gulp.task( tasksList.browserSync, doBrowserSync );
+
+// To compile stylesheet. "gulp style" in CLI.
 gulp.task( tasksList.style, loadStyle );
 
-/**
- * Task to compile JavaScript.
- */
+// To compile script. "gulp js" in CLI.
 gulp.task( tasksList.js, loadJavaScript );
 
-/**
- * Task to watch for file changes.
- */
+// To watch for file changes. "gulp watch" in CLI.
 gulp.task( tasksList.watch, doWatch );
 
-/**
- * Task to run default tasks.
- */
+// Task to run default tasks. "gulp" in CLI.
 gulp.task( tasksList.default, gulp.parallel( tasksDefault ) );
